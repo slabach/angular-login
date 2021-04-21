10a8aa424317ea1daa5a0b1084e3d415d99ef0d3
Byron Slabach <byron.slabach@gmail.com>
2021-04-21 13:04:47 -0400

    angular front end login form/handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable, throwError } from 'rxjs';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ango-ui';
+  title = 'ango-ui'
+  // init username and password 
+  log_user = ''
+  log_pwd = ''
+
+  // init http client interface
+  constructor(
+    private httpClient: HttpClient
+  ) {}
+
+  // handle submit login request
+  async submitLogin(){
+		// get time for token comparison
+    let today = new Date()
+    let cur_hour = today.getHours().toString().padStart(2, '0')
+    let cur_minute = today.getMinutes().toString().padStart(2, '0')
+    let token = `${cur_hour}${cur_minute}`
+    let token_int = parseInt(token)
+
+    // POST request to api
+    await this.httpClient.post('/api/login', {
+      username: this.log_user,
+      password: this.log_pwd,
+      timestamp: token_int
+    }).subscribe({
+      // if success response, alert success
+      next: resp => {        
+        console.log(resp)
+          Swal.fire({
+            title: 'Success!',
+            text: 'Do you want to continue',
+            icon: 'success',
+            confirmButtonText: 'Cool'
+          }).then((result) => {
+            // if confirm button selected, navigate to next page
+            if (result.isConfirmed) {
+              document.location.href = 'http://onecause.com'
+            }
+          })
+      },
+      // if error, display alert with error message from API
+      error: error => {
+        console.log(error)
+          Swal.fire({
+            title: 'Error!',
+            text: error.error.message,
+            icon: 'error',
+            confirmButtonText: 'Shucks'
+          })
+      }
+    })
+
+    // reset username and password fields on DOM
+    this.log_user = ''
+    this.log_pwd = ''
+  }
 }
+
+// {
+//   "username": "c137@onecause.com",
+//   "password": "#th@nH@rm#y#r!$100%D0p#"
+// }
