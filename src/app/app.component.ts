03769892514ae2e8f55622ad6d116bde2644b1f6
Byron Slabach <byron.slabach@gmail.com>
2021-04-21 16:50:12 -0400

    remove rxjs dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-import { Observable, throwError } from 'rxjs';
 import Swal from 'sweetalert2'
 
 @Component({
